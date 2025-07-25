a7e3aaa83b395a0be666b72126db24e94dd6b5ed
LowkeyFract <lowkeyfract@gmail.com>
2025-07-25 21:21:16 +0100

    Fix

--- loader.lua
+++ loader.lua
@@ -1,9 +1,7 @@
-local baseUrl = "https://raw.githubusercontent.com/LowkeyFractAd/Universal-Hub/master/"
+local baseUrl = "https://raw.githubusercontent.com/LowkeyFractAd/Universal-Hub/main/"
 
 local Window = assert(loadstring(game:HttpGet(baseUrl .. "RayfieldMain.lua")))()
 local TrollTab = assert(loadstring(game:HttpGet(baseUrl .. "Tabs/TrollTab.lua")))()
 TrollTab(Window)
 local PlayerSettingsTab = assert(loadstring(game:HttpGet(baseUrl .. "Tabs/PlayerSettingsTab.lua")))()
 PlayerSettingsTab(Window)
-
--- test
