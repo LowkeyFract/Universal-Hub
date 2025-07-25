088e4b8eb8223d6921bf75c4435cb98f9e1c31fb
LowkeyFract <lowkeyfract@gmail.com>
2025-07-25 21:32:22 +0100

    Fix

--- loader.lua
+++ loader.lua
@@ -1,18 +1,7 @@
-local baseUrl = "https://raw.githubusercontent.com/LowkeyFractAd/Universal-Hub/main/"
+local baseUrl = "https://raw.githubusercontent.com/LowkeyFract/Universal-Hub/main/"
 
-local RayfieldMainCode = game:HttpGet(baseUrl .. "RayfieldMain.lua")
-local f1, err1 = loadstring(RayfieldMainCode)
-assert(f1, "Error loading RayfieldMain.lua: " .. err1)
-local Window = f1()
-
-local TrollTabCode = game:HttpGet(baseUrl .. "Tabs/TrollTab.lua")
-local f2, err2 = loadstring(TrollTabCode)
-assert(f2, "Error loading TrollTab.lua: " .. err2)
-local TrollTabFunc = f2
-TrollTabFunc(Window)
-
-local PlayerSettingsTabCode = game:HttpGet(baseUrl .. "Tabs/PlayerSettingsTab.lua")
-local f3, err3 = loadstring(PlayerSettingsTabCode)
-assert(f3, "Error loading PlayerSettingsTab.lua: " .. err3)
-local PlayerSettingsTabFunc = f3
-PlayerSettingsTabFunc(Window)
+local Window = assert(loadstring(game:HttpGet(baseUrl .. "RayfieldMain.lua")))()
+local TrollTab = assert(loadstring(game:HttpGet(baseUrl .. "Tabs/TrollTab.lua")))()
+TrollTab(Window)
+local PlayerSettingsTab = assert(loadstring(game:HttpGet(baseUrl .. "Tabs/PlayerSettingsTab.lua")))()
+PlayerSettingsTab(Window)
